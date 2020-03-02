bbd3f8f6104a3416ce5f612a5e34bca0bdbdd79d
Briam Martinez <briammartinez@gmail.com>
2020-03-02 23:48:14 +0100

    Added swagger with the initial doc of the api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const swaggerUI = require('swagger-ui-express');
+
 const config = require('../config.js');
 const user = require('./components/user/network');
 
@@ -8,8 +10,11 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const swaggerDoc = require('./swagger.json');
+
 // ROUTER
 app.use('/api/user', user)
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 app.listen(config.api.port, () => {
     console.log('Listening on port', config.api.port);
